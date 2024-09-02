2706a7431c36d15673bac758ca7aa964dbdcf4f7
Sanjeeva Chitlapalli <sanjeeva.chitlapalli@thoughtclan.com>
2024-09-02 14:15:07 +0530

    updated mojito files

--- mojito/ex2/1.js
+++ mojito/ex2/1.js
@@ -1,3 +1,5 @@
+
 function treatment(){
+    // This code will run once the test activates & the user is bucketed
     alert('Hi Google user!');
 }
--- mojito/ex2/trigger.js
+++ mojito/ex2/trigger.js
@@ -1,10 +1,7 @@
-function trigger(test){
-    // Activate the experiment for all users
-    test.activate();
 
-    // OR activate based on a query parameter (e.g., ?experiment=ex2)
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('experiment') === 'ex2') {
-        test.activate();
-    }
+function trigger(test){
+    // Only activate and bucket users into the experiment if they come from Google
+    const isGroupA = Math.random() < 0.8;
+    // (Runs as soon as the container is loaded)
+    if (isGroupA) test.activate();
 }
